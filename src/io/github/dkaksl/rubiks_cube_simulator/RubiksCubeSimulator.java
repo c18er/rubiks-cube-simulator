52318e3db11639900953295f107ad72b88aad548
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-02-02 21:52:26 +0900

    added UI stub

--- src/io/github/dkaksl/rubiks_cube_simulator/RubiksCubeSimulator.java
+++ src/io/github/dkaksl/rubiks_cube_simulator/RubiksCubeSimulator.java
@@ -7,27 +7,63 @@
  */
 package io.github.dkaksl.rubiks_cube_simulator;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
  * @author Amani Kilumanga
  *
  */
-public class RubiksCubeSimulator {
+public class RubiksCubeSimulator extends JFrame implements ActionListener {
+
+	private final JPanel centerPanel;
+
+	private static Cube cube;
+
+	public RubiksCubeSimulator(int width, int height) {
+		centerPanel = generateCenterPanel();
+
+		this.setLayout(new BorderLayout());
+		this.setSize(width, height);
+		this.getContentPane().add(centerPanel, BorderLayout.CENTER);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+
+		cube = new Cube();
+	}
+
+	private static JPanel generateCenterPanel() {
+		JPanel jPanel = new JPanel();
+
+		return jPanel;
+	}
+
+	private static void generateMainWindow() {
+		RubiksCubeSimulator simulator = new RubiksCubeSimulator(600, 600);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		// if (e.equals(objectA)) { // do stuff }
+
+		/*
+		 * Cube cube = new Cube(); cube.printCube(); // cube.turnUp(); cube.printCube();
+		 */
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Cube cube = new Cube();
-		cube.printCube();
-
-		// cube.turnUp();
-		cube.turnBack();
-		cube.turnBack();
-		cube.turnBack();
-		cube.turnBack();
-		cube.printCube();
-
-		// TODO: make UI (to help debugging)
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				generateMainWindow();
+			}
+		});
 	}
 
 }
