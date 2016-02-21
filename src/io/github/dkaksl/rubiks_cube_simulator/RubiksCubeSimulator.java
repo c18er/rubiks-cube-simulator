5a1d0f73def8eec0e47227945efd6bc0223b142d
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-02-21 18:16:30 +0900

    added textarea to see cube sides

--- src/io/github/dkaksl/rubiks_cube_simulator/RubiksCubeSimulator.java
+++ src/io/github/dkaksl/rubiks_cube_simulator/RubiksCubeSimulator.java
@@ -11,8 +11,10 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * @author Amani Kilumanga
@@ -20,25 +22,83 @@ import javax.swing.JPanel;
  */
 public class RubiksCubeSimulator extends JFrame implements ActionListener {
 
+	private final JPanel topPanel;
 	private final JPanel centerPanel;
+
+	private static JButton upButton;
+	private static JButton backButton;
+	private static JButton leftButton;
+	private static JButton frontButton;
+	private static JButton rightButton;
+	private static JButton downButton;
+
+	private static JTextArea cubeView;
 
 	private static Cube cube;
 
 	public RubiksCubeSimulator(int width, int height) {
+		topPanel = generateTopPanel();
 		centerPanel = generateCenterPanel();
 
 		this.setLayout(new BorderLayout());
 		this.setSize(width, height);
+		this.getContentPane().add(topPanel, BorderLayout.NORTH);
 		this.getContentPane().add(centerPanel, BorderLayout.CENTER);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
 
 		cube = new Cube();
+
+		updateCubeView();
 	}
 
-	private static JPanel generateCenterPanel() {
+	private static void updateCubeView() {
+		String[][] view = cube.getFlatCube();
+
+		cubeView.setText("");
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 12; j++) {
+				cubeView.append(view[i][j]);
+			}
+			cubeView.append("\n");
+		}
+	}
+
+	private JPanel generateTopPanel() {
 		JPanel jPanel = new JPanel();
+
+		upButton = new JButton("up");
+		backButton = new JButton("back");
+		leftButton = new JButton("left");
+		frontButton = new JButton("front");
+		rightButton = new JButton("right");
+		downButton = new JButton("down");
+
+		upButton.addActionListener(this);
+		backButton.addActionListener(this);
+		leftButton.addActionListener(this);
+		frontButton.addActionListener(this);
+		rightButton.addActionListener(this);
+		downButton.addActionListener(this);
+
+		jPanel.add(upButton);
+		jPanel.add(backButton);
+		jPanel.add(leftButton);
+		jPanel.add(frontButton);
+		jPanel.add(rightButton);
+		jPanel.add(downButton);
+
+		return jPanel;
+	}
+
+	private JPanel generateCenterPanel() {
+		JPanel jPanel = new JPanel();
+
+		cubeView = new JTextArea();
+		cubeView.setEditable(false);
+
+		jPanel.add(cubeView);
 
 		return jPanel;
 	}
@@ -48,11 +108,20 @@ public class RubiksCubeSimulator extends JFrame implements ActionListener {
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		// if (e.equals(objectA)) { // do stuff }
-
-		/*
-		 * Cube cube = new Cube(); cube.printCube(); // cube.turnUp(); cube.printCube();
-		 */
+		if (e.getSource() == (upButton)) {
+			cube.turnUp();
+		} else if (e.getSource() == backButton) {
+			cube.turnBack();
+		} else if (e.getSource() == leftButton) {
+			cube.turnLeft();
+		} else if (e.getSource() == frontButton) {
+			cube.turnFront();
+		} else if (e.getSource() == rightButton) {
+			cube.turnRight();
+		} else if (e.getSource() == downButton) {
+			cube.turnDown();
+		}
+		updateCubeView();
 	}
 
 	/**
